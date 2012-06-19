7b426dd164b6437a1b35d628d03d17f9a2167870
Bryan Rite <bryan@bryanrite.com>
2012-06-19 11:23:13 -0700

    Basic sintra skeleton with sequel.

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'sequel'
+gem 'sinatra-sequel'
+gem 'json'
+gem 'sqlite3'
+
+group :development do
+  gem 'shotgun'
+end
---
+++ application.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/sequel'
+require "sinatra/config_file"
+
+config_file 'config/config.yml'
+
+# Autoload Directories
+autoload = %w(models)
+autoload.each do |directory|
+  Dir[File.dirname(__FILE__) + "/#{directory}/*.rb"].each { |file| require file }
+end
+
+get '/' do
+  "Welcome to Tropo Customer Survey App POC"
+end
